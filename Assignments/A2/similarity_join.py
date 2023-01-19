3a237c65b060d298857ef25c987e664a48368780
Darshaun Kandasamy Govindhraj <darshaun@gmail.com>
2023-01-19 13:02:34 -0800

    assignment 2 full code

--- Assignments/A2/similarity_join.py
+++ Assignments/A2/similarity_join.py
@@ -12,23 +12,79 @@ class SimilarityJoin:
 		""" 
 		df[cols] = df[cols].fillna('')
 		df['joinKey'] = df[cols].apply(' '.join, axis=1)
-		df['joinKey'] = df['joinKey'].apply(lambda x: list(filter(None, re.split(r'\W+', x))))
+		df['joinKey'] = df['joinKey'].apply(lambda x: list(
+			filter(
+				None, 
+				list(
+					set(
+						[x.lower() for x in re.split(r'\W+', x.rstrip('.'))]
+					)
+				)
+			)
+		))
 		return df
 	
 	def filtering(self, df1, df2):
 		"""
 			Write your code!
 		"""
+		df1_short = df1[['id', 'joinKey']]
+		df2_short = df2[['id', 'joinKey']]
+		df1_exploded = df1_short.set_index('id') \
+								.apply(pd.Series.explode) \
+								.reset_index() \
+								.set_index('joinKey')
+		df2_exploded = df2_short.set_index('id') \
+								.apply(pd.Series.explode) \
+								.reset_index() \
+								.set_index('joinKey')
+
+		common_tokens = df1_exploded.join(df2_exploded, how='inner', lsuffix='1', rsuffix='2')	
+
+		common_tokens_with_count = common_tokens.groupby(['id1', 'id2']) \
+										.size() \
+										.reset_index(name='r_intersect_s')
+
+		cand_df = common_tokens_with_count.merge(df1_short.rename(columns={'id':'id1', 'joinKey':'joinKey1'}), on='id1') \
+									.merge(df2_short.rename(columns={'id':'id2', 'joinKey':'joinKey2'}), on='id2') \
+									[['id1', 'joinKey1', 'id2', 'joinKey2']]
+
+		return cand_df
 	  
 	def verification(self, cand_df, threshold):
 		"""
 			Write your code!
 		"""
+		cand_df['jaccard'] = cand_df.apply(
+			lambda x: (len(list(set(x[1]).intersection(set(x[3])))) / len(list(set(x[1]).union(set(x[3]))))), 
+			axis=1
+		)
+		result_df = cand_df[cand_df['jaccard'] >= threshold]
+		return result_df
 		
 	def evaluate(self, result, ground_truth):
 		"""
 			Write your code!
 		"""
+		identified_matching_pairs = len(result)
+		truly_matching_pairs_in_dataset = len(ground_truth)
+		truly_matching_pairs = 0
+
+		ground_truth_dict = {}
+		for df1_id, df2_id in ground_truth:
+			ground_truth_dict[df1_id] = df2_id
+
+		for df1_id, df2_id in result:
+			if (df1_id in ground_truth_dict) and (df2_id == ground_truth_dict[df1_id]):
+				truly_matching_pairs += 1
+		
+		precision = truly_matching_pairs / identified_matching_pairs
+
+		recall = truly_matching_pairs / truly_matching_pairs_in_dataset
+
+		fmeasure = (2 * precision * recall) / (precision + recall)
+
+		return (precision, recall, fmeasure)
 		
 	def jaccard_join(self, cols1, cols2, threshold):
 		new_df1 = self.preprocess_df(self.df1, cols1)
@@ -47,10 +103,12 @@ class SimilarityJoin:
 
 if __name__ == "__main__":
 	er = SimilarityJoin("Amazon_sample.csv", "Google_sample.csv")
+	# er = SimilarityJoin("Amazon.csv", "Google.csv")
 	amazon_cols = ["title", "manufacturer"]
 	google_cols = ["name", "manufacturer"]
 	result_df = er.jaccard_join(amazon_cols, google_cols, 0.5)
 
 	result = result_df[['id1', 'id2']].values.tolist()
 	ground_truth = pd.read_csv("Amazon_Google_perfectMapping_sample.csv").values.tolist()
+	# ground_truth = pd.read_csv("Amazon_Google_perfectMapping.csv").values.tolist()
 	print ("(precision, recall, fmeasure) = ", er.evaluate(result, ground_truth))
